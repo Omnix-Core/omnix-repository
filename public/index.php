59c53c4d86ede9e74414d60a0946618300bad250
Waliidd01 <1814253@alu.murciaeduca.es>
2026-01-29 13:18:54 +0100

    Correcciones

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
     session_start();
 
     require_once __DIR__ . '/../config/params.php';
-    require_once __DIR__ . '/../app/core/Router.php';
+    require_once __DIR__ . '/../app/libs/Router.php';
 
     $router = new Router();
     $router->run();
