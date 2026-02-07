a97f4f3025572eb7e1928d9a964ad28a2b9f49d2
Cristian López <1923154@alu.murciaeduca.es>
2026-02-07 11:49:23 +0100

    Cambios en el header y footer

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,25 +1,22 @@
 <?php
 
-require_once __DIR__ . '/../libs/Database.php';
+require_once __DIR__ . '/../models/ProductRepository.php';
 
-class HomeController{
-    public function index(){
-        try {
-            $db = Database::getInstance();
-            $pdo = $db->getConnection();
+class HomeController
+{
+    private $productRepo;
 
-            $datos = [
-                'titulo' => 'Bienvenido a Omnix',
-                'texto'  => 'Los mejores precios en tecnología.'
-            ];
+    public function __construct()
+    {
+        $this->productRepo = new ProductRepository();
+    }
 
-            require_once __DIR__ . '/../views/home/index.php';
-
-        } catch (Throwable $e) {
-
-            error_log('Error en HomeController: ' . $e->getMessage());
-
-            require_once __DIR__ . '/../views/errors/500.php';
-        }
+    public function index()
+    {
+        $productos = $this->productRepo->findAll();
+        
+        extract(['productos' => $productos]);
+        
+        require_once __DIR__ . '/../views/home/index.php';
     }
 }
